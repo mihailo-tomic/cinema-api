106388cb9e146070f488f5bafa1011aefb0e4b89
Mihailo Tomic <mihailo@ballab.com>
2017-09-27 20:04:04 +0200

    Alter a column name in DbConstants

--- src/main/java/com/mihailo/cinema/common/DbConstants.java
+++ src/main/java/com/mihailo/cinema/common/DbConstants.java
@@ -33,7 +33,7 @@ public class DbConstants {
         public static final String ID = "id";
 
         public static final String NAME = "name";
-        public static final String CHARACTER = "character";
+        public static final String CHARACTER = "character_name";
 
     }
 
