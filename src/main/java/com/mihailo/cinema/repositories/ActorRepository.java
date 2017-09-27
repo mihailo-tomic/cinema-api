548173a993077e1c03585584febb9249eedbb28b
Mihailo Tomic <mihailo@ballab.com>
2017-09-27 20:55:54 +0200

    Add ActorRepository

---
+++ src/main/java/com/mihailo/cinema/repositories/ActorRepository.java
@@ -0,0 +1,8 @@
+package com.mihailo.cinema.repositories;
+
+import com.mihailo.cinema.model.Actor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+@RestResource(exported = false)
+public interface ActorRepository extends CrudRepository<Actor, Long> {}
